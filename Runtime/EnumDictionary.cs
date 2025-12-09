14db9c9858955247c694bfe49e23aa1bf6df0173
EOppedal <erlend.oppedal@gmail.com>
2025-12-09 14:40:48 +0100

    Refactor EnumDictionary to validate construction values and not parameters

--- Runtime/EnumDictionary.cs
+++ Runtime/EnumDictionary.cs
@@ -4,13 +4,8 @@ using System.Collections.Generic;
 public sealed record EnumDictionary<TEnum, TValue> where TEnum : Enum {
     private readonly Dictionary<TEnum, TValue> _dictionary = new();
 
-    public EnumDictionary(params TValue[] values) {
-        var enumValues = (TEnum[])Enum.GetValues(typeof(TEnum));
-
-        for (var i = 0; i < enumValues.Length; i++) {
-            _dictionary.Add(enumValues[i], values[i]);
-        }
+    public TValue this[TEnum key] {
+        get => _dictionary[key];
+        set => _dictionary[key] = value; 
     }
-
-    public TValue this[TEnum key] => _dictionary[key];
 }
