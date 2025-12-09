dd69ffea279217bbe2d65d3b8c50dcd307a09c23
EOppedal <erlend.oppedal@gmail.com>
2025-12-09 11:54:07 +0100

    Initial commit

---
+++ Runtime/EnumDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public sealed record EnumDictionary<TEnum, TValue> where TEnum : Enum {
+    private readonly Dictionary<TEnum, TValue> _dictionary = new();
+
+    public EnumDictionary(params TValue[] values) {
+        var enumValues = (TEnum[])Enum.GetValues(typeof(TEnum));
+
+        for (var i = 0; i < enumValues.Length; i++) {
+            _dictionary.Add(enumValues[i], values[i]);
+        }
+    }
+
+    public TValue this[TEnum key] => _dictionary[key];
+}
